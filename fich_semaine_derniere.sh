e1d6528961af019140666873cf3a693ac2c2745d
Sorbonne <hatun.ozgu@gmail.com>
2018-12-03 23:37:32 +0100

    Ajout de l'affichage des fichiers modifiés il y a moins d'une semaine

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -8,3 +8,7 @@ echo "Bienvenue $USERNAME ! Nous sommes le $jour et il est actuellement $heure."
 read -p "Quel répertoire vous intéresse aujourd'hui ?" rep
 echo "Le répertoire fourni est bien : $rep"
 
+cd $rep
+echo "Tous les fichiers de $rep modifiés il y a moins d'une semaine sont :"
+find . -type f -mtime -7
+
